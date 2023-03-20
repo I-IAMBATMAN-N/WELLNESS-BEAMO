6b35c5d47ade04b5e3da4c9e4d720ee3a61e8aeb
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-03-20 18:44:19 +0100

    [CREATE]Slider component functionality
    for treatment.html slider component

--- script.js
+++ script.js
@@ -17,6 +17,55 @@ sliderContainer.forEach((sliderContainer) => {
   });
   //display default current dot
   const dots = dotContainer.querySelectorAll(".slider-dot");
-  // dots[counter].style.backgroundColor = "#6e825f";
   dots[counter].classList.add("active");
+});
+
+let currSlides;
+let currDots;
+
+sliderContainer.forEach((sliderComponent) => {
+  let currSlide = 0;
+  sliderComponent
+    .querySelectorAll(".slider-controls")
+    .forEach((sliderControl) => {
+      sliderControl.addEventListener("click", function (event) {
+        console.log("sliderControl", sliderControl);
+        //
+        currSlides = event.target
+          .closest(".slider-controls")
+          .closest(".treatment-slider").children;
+
+        currDots =
+          event.target.closest(".slider-controls").children[1].children;
+        //
+        //define slide direction
+        if (event.target.name === "chevron-forward-outline") {
+          //
+          if (currSlide === currSlides.length - 2) {
+            currSlide = 0;
+          } else {
+            currSlide++;
+          }
+        } else if (event.target.name === "chevron-back-outline") {
+          if (currSlide === 0) {
+            currSlide = currSlides.length - 2;
+          } else {
+            currSlide--;
+          }
+        }
+        //
+        //shift all current slides on click
+        for (let i = 0; i < currSlides.length - 1; i++) {
+          currSlides[i].style.transform = `translateX(${
+            140 * (i - currSlide)
+          }%)`;
+        }
+        //reset active status for all dots
+        for (let i = 0; i < currDots.length; i++) {
+          currDots[i].classList.remove("active");
+        }
+        //display current active dot
+        currDots[currSlide].classList.add("active");
+      });
+    });
 });
