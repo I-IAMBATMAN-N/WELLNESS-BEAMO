9a43e018b18a4090fdfe8dc30b3d70109c51f48a
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-03-21 12:03:40 +0100

    [CREATE]Responsive design - mobile
    Hamburger menu navigation
    index.html
    - HTML structure
    style.css
    - stylings
    script.js
    -menu functionality

--- script.js
+++ script.js
@@ -69,3 +69,15 @@ sliderContainer.forEach((sliderComponent) => {
       });
     });
 });
+
+const menuIcons = document.querySelectorAll(".mobile-menu-icon");
+const navList = document.querySelector(".nav-list");
+
+menuIcons.forEach((icon) =>
+  icon.addEventListener("click", function (event) {
+    menuIcons.forEach((icon) => {
+      icon.classList.toggle("active");
+    });
+    navList.classList.toggle("active");
+  })
+);
