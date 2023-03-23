e8824948981f0bbc0642b9322e538df835311a0a
JB <93525600+I-IAMBATMAN-N@users.noreply.github.com>
2023-03-23 15:57:54 +0100

    [CREATE] Treatment options
    treatment.html
    - sections moved (Dermaceutic below Cosmetic)
    - Treatment options related classes added
          - treatment-options (multiple options inside span element)
          - multiple prices in span element inside related treatment-price element
          - options-controls (option change related controls)
    
    style.css
    - treatment options related stylings
    script.js
    - initial options display
    - options related function (change option and price onclick)
    
    images
     - added missing image for Manicure card

--- script.js
+++ script.js
@@ -11,3 +11,96 @@ menuIcons.forEach((icon) =>
     navList.classList.toggle("active");
   })
 );
+
+/*-------------------------------------------------------------------------------------------------------*/
+let currOption = 0;
+
+const optionalControls = document.querySelectorAll(
+  ".options-controls .slider-btn"
+);
+
+const options = document.querySelectorAll(
+  ".treatment-card.optional .treatment-options"
+);
+
+const optionsPrices = document.querySelectorAll(
+  ".treatment-card.optional .treatment-price"
+);
+
+options.forEach((options) => {
+  options.querySelectorAll("span").forEach((span, index) => {
+    if (index === currOption) {
+      span.style.display = "inline-block";
+    } else if (index !== currOption) {
+      span.style.display = "none";
+    }
+  });
+});
+
+optionsPrices.forEach((optionsPrice) => {
+  optionsPrice.querySelectorAll("span").forEach((span, index) => {
+    if (index === currOption) {
+      span.style.display = "inline-block";
+    } else if (index !== currOption) {
+      span.style.display = "none";
+    }
+  });
+});
+
+// options.forEach((option, index) => {
+//   if (index === currOption) {
+//     option.style.display = "inline-block";
+//   } else if (index !== currOption) {
+//     option.style.display = "none";
+//   }
+// });
+
+// optionsPrices.forEach((optionPrice, index) => {
+//   if (index === currOption) {
+//     optionPrice.style.display = "block";
+//   } else if (index !== currOption) {
+//     optionPrice.style.display = "none";
+//   }
+// });
+
+optionalControls.forEach((control) => {
+  let currOption = 0;
+  control.addEventListener("click", function (event) {
+    //
+    const optioncardOptions = event.target
+      .closest(".treatment-card")
+      .children[1].children[1].querySelectorAll("span");
+    const optionPrices = event.target
+      .closest(".treatment-card")
+      .children[1].children[3].children[1].querySelectorAll("span");
+    //
+    if (event.target.name === "chevron-forward-outline") {
+      if (currOption === options.length) {
+        currOption = 0;
+      } else if (currOption !== options.length) {
+        currOption++;
+      }
+    } else if (event.target.name === "chevron-back-outline") {
+      if (currOption === 0) {
+        currOption = options.length - 1;
+      } else if (currOption !== 0) {
+        currOption--;
+      }
+    }
+
+    optioncardOptions.forEach((option, index) => {
+      if (index === currOption) {
+        option.style.display = "inline-block";
+      } else if (index !== currOption) {
+        option.style.display = "none";
+      }
+    });
+    optionPrices.forEach((optionPrice, index) => {
+      if (index === currOption) {
+        optionPrice.style.display = "inline-block";
+      } else if (index !== currOption) {
+        optionPrice.style.display = "none";
+      }
+    });
+  });
+});
